16cc75fa871b67fb5c2cadb5cb60fc934a7fc8d8
Dinaed <Dinaedandr@gmail.com>
2022-02-13 20:27:25 +0200

    could be the end part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HoverEffectDirective } from './directive/hover-effect.directive';
-import { MinMaxPipe } from './pipes/min-max.pipe';
 import{ MyFilterPipe } from './pipes/myFilter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFilterPipe,
-    HoverEffectDirective,
-    MinMaxPipe
+    HoverEffectDirective
   ],
   imports: [
     BrowserModule,
