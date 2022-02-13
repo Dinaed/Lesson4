a4eb3aa0930b625845e2ca1b7c6c55f31bc3a732
Dinaed <Dinaedandr@gmail.com>
2022-02-13 20:26:37 +0200

    could be the end

--- src/app/pipes/myFilter.pipe.ts
+++ src/app/pipes/myFilter.pipe.ts
@@ -1,3 +1,4 @@
+import { invalid } from '@angular/compiler/src/render3/view/util';
 import { Pipe, PipeTransform } from '@angular/core';
 import { User } from '../models/user';
 
@@ -6,12 +7,30 @@ import { User } from '../models/user';
 })
 export class MyFilterPipe implements PipeTransform {
 
-  transform(value: any[], filter: any): User[] | any {
+  transform(value: any[], filter:any , minMax:any): User[] | any {
     if(!value)return null;
-    if(!filter)return value;
-    filter = filter.toLowerCase();
-    return value.filter(item =>{
-      return JSON.stringify(item).toLowerCase().includes(filter);
-    });
+    else if(!filter && !minMax)return value;
+    else if(filter){
+      filter = filter.toLowerCase();
+      return value.filter(item =>{ 
+        return JSON.stringify(item).toLowerCase().includes(filter);
+      });
+    }
+    else if(minMax){
+      minMax = minMax.toLowerCase().trim().split(' ');
+      console.log(minMax);
+      return value.filter(item => {
+        if(minMax[0]=== '<'){
+          if(item.age < Number(minMax[1])){          
+            return item;
+          }
+        }
+        else if(minMax[0]=== '>'){
+            if(item.age > minMax[1]){
+              return item;
+            }
+          }      
+      });
+    }
   }
 }
